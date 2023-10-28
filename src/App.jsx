109d0c86a57c102c4081c5d245dccd5fa2abf465
nomayen <nomayen.ohin@gmail.com>
2023-10-28 23:49:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+     <h1>Memorable Water Battle</h1>
+     <Header></Header>
+    </>
+  )
+}
+
+export default App
